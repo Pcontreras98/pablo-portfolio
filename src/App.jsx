1bd79910af93d436a6c08bb25e391fcf0952e598
Pablo Martin Contreras Niño <martin_conterasn@hotmail.com>
2026-06-13 18:46:53 -0600

    Add speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { useSmoothAnchorScroll } from './hooks/useSmoothAnchorScroll'
 import { DocumentHead } from './components/DocumentHead'
 import { Navbar } from './components/Navbar'
@@ -34,6 +35,7 @@ export default function App() {
         </main>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </div>
     </div>
   )
